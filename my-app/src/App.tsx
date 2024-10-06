051bb1b36b3c57efbbf34d7bbc1342766f537356
Edder Hernandez <eddeh17963@gmail.com>
2024-10-06 03:12:50 -0400

    rearanged files in front end

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,28 +1,41 @@
 //import '../src/app2.css';  // or './App.css'
 //import "./";
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 // import Header from './components/Header';
 //import Sidebar from './components/Sidebar';
 import MainContent from './components/MainContent';
 import MainPage from './components/MainPage';
 //import './App.css';
 import VerticalNavbar from './components/VerticalNav';
+import Home from './pages/Home';
 
 const App: React.FC = () => {
   return (
-    <div className="flex h-screen">
-      {/* Navbar on the left */}
-      <VerticalNavbar />
+  <Router>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
+    </Routes>
+  </Router>
 
-      {/* Main content area */}
-      <div className="flex-1 ml-64 p-4 bg-fuchsia-50">
-        <div className="rounded-2xl border-8 border-solid border-stone-300 p-4">
-         {/*<!<MainPage />*/}
-          <MainContent />
-        </div>
-      </div>
-    </div>
+
+
+
+
+    // <div className="flex h-screen">
+    //   {/* Navbar on the left */}
+    //   <VerticalNavbar />
+
+    //   {/* Main content area */}
+    //   <div className="flex-1 ml-64 p-4 bg-fuchsia-50">
+    //     <div className="rounded-2xl border-8 border-solid border-stone-300 p-4">
+    //      {/*<!<MainPage />*/}
+    //       <MainContent />
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
 
